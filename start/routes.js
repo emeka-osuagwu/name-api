1e1aac7a73d0ef4aa92d58fc4584323e6fe6b441
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2018-11-25 08:12:59 +0100

    add get black list route

--- start/routes.js
+++ start/routes.js
@@ -21,6 +21,7 @@ Route.get("/", "NameController.index");
 Route.group(() => {
     Route.get("/", "NameController.index");
     Route.get("/white_list", "NameController.getWhiteList");
+    Route.get("/black_list", "NameController.getWhiteList");
     Route.post("create", "NameController.create");
     Route.post(":id/update", "NameController.update");
     Route.post(":id/delete", "NameController.delete");
